d849909d4f3aaa7976955ad00f5bb732f41949d2
Daniel Rygaard <daniel.rygaard@weknowit.se>
2024-03-05 16:10:55 +0100

    Added clang formatting

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,6 +7,7 @@ return {
 				null_ls.builtins.formatting.stylua,
         null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.formatting.clang_format,
 			},
 		})
 		vim.keymap.set("n", "<space>gf", vim.lsp.buf.format, {})
