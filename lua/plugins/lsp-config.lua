13f04c43b68f0a64e5c4c665c5d423cfdef7443b
Danryg <danryg@student.chalmers.se>
2024-05-04 11:32:47 +0200

    Changed theme and some other stuff

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -8,7 +8,7 @@ return {
 	{
 		"williamboman/mason-lspconfig.nvim",
 		lazy = false,
-		ensure_installed = { "lua_ls", "tsserver", "html", "clangd" },
+		ensure_installed = { "lua_ls", "tsserver", "html", "clangd", "pyright", "black", "tailwindcss" },
 		opts = {
 			auto_install = true,
 		},
@@ -18,7 +18,7 @@ return {
 		lazy = false,
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      
+
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
@@ -29,21 +29,42 @@ return {
 			lspconfig.html.setup({
 				capabilities = capabilities,
 			})
-      lspconfig.clangd.setup({
+			lspconfig.clangd.setup({
+				filetypes = { "c", "cpp" },
+
+				cmd = {
+					"clangd",
+					"--header-insertion=never",
+					"--suggest-missing-includes",
+          "D:/avrgcc/avr-gcc-13.2.0-x64-windows/include",
+				},
+				capabilities = capabilities,
+			})
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
+
+      lspconfig.tailwindcss.setup({
+        cmd = { "tailwindcss-language-server", "--stdio" },
+        filetypes = { "html", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact", "svelte", "vue" },
         capabilities = capabilities,
       })
-      
+      lspconfig.eslint.setup({
+        capabilities = capabilities,
+      })
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
 			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
 			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
 		end,
-	},{
-    "williamboman/nvim-lsp-installer",
-    config = function ()
-      require("nvim-lsp-installer").setup {}
-    end
-  }
+	},
+	{
+		"williamboman/nvim-lsp-installer",
+		config = function()
+			require("nvim-lsp-installer").setup({})
+		end,
+	},
 }
