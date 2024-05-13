41bccb9e26d4b252b6fd480fb140a314b095b5b2
Danryg <danryg@student.chalmers.se>
2024-05-13 20:31:46 +0200

    Updated lintign and formatting plugins

--- lua/daniel/plugins/lazygit.lua
+++ lua/daniel/plugins/lazygit.lua
@@ -1,23 +1,19 @@
 return {
 	"kdheepak/lazygit.nvim",
-
-	config = function()
-		-- nvim v0.8.0
-		require("lazy").setup({
-			{
-				"kdheepak/lazygit.nvim",
-				cmd = {
-					"LazyGit",
-					"LazyGitConfig",
-					"LazyGitCurrentFile",
-					"LazyGitFilter",
-					"LazyGitFilterCurrentFile",
-				},
-				-- optional for floating window border decoration
-				dependencies = {
-					"nvim-lua/plenary.nvim",
-				},
-			},
-		})
-	end,
+	cmd = {
+		"LazyGit",
+		"LazyGitConfig",
+		"LazyGitCurrentFile",
+		"LazyGitFilter",
+		"LazyGitFilterCurrentFile",
+	},
+	-- optional for floating window border decoration
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	-- setting the keybinding for LazyGit with 'keys' is recommended in
+	-- order to load the plugin when the command is run for the first time
+	keys = {
+		{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
+	},
 }
