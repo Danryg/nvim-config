2782eb563969de6e56be1308c5875b245115b6cb
Danryg <danryg@student.chalmers.se>
2024-05-13 17:48:26 +0200

    Removed: unused pugins and fixed autosessions

--- lua/daniel/plugins/auto-sessions.lua
+++ lua/daniel/plugins/auto-sessions.lua
@@ -5,13 +5,12 @@ return {
 
 		auto_session.setup({
 			auto_restore_enabled = false,
-			auto_save_enabled = true,
-      auto_sessions_supress_dirs = {"~/", "~/Dev/", "~/Desktop/", "~/Documents/", "~/Downloads/", "~/Pictures/", "~/Videos/"},
+			auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
 		})
 
-    local keymap = vim.keymap
+		local keymap = vim.keymap
 
-    keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc="Restore session for cwd" })
-    keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc="Save session for cwd" })
+		keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
+		keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
 	end,
 }
